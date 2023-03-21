bdf526a49b2654e67aed86f356a190a8c2a6f7a9
kris1486 <84494688+kris1486@users.noreply.github.com>
2023-03-21 21:03:21 +0800

    Update tdlist.js

--- tdlist.js
+++ tdlist.js
@@ -3,49 +3,44 @@ const input = document.getElementById("inputToDo");
 const toDoItems = document.getElementById("to-do-List");
 
 
-
 addBtn.addEventListener("click", function(event){
 
   if(event) addItem();
 
   function addItem(){            
-      var divChild = document.createElement("div");
-      var inputText = document.createElement("p");
-      var checkIcon = document.createElement("input");
-      var editIcon = document.createElement("button");
-      var trashIcon = document.createElement("button");
+      const divInput = document.createElement("div");
+      const inputText = document.createElement("p");
+      const checkIcon = document.createElement("input");
+      const editIcon = document.createElement("button");
+      const trashIcon = document.createElement("button");
   
-      divChild.className = "input-group";
-    
+      divInput.className = ("container");      
       inputText.id = "inputValue";
       inputText.innerHTML = input.value;
-      divChild.appendChild(inputText);
+      divInput.append(inputText);
 
 
       checkIcon.id = "done";
       checkIcon.type = "checkbox";
       checkIcon.addEventListener("click", function() {
-        paragraph.contentEditable = true;
-        paragraph.style.backgroundColor = "#dddbdb";
+        inputText.style.textDecorationStyle = "line-through";
       });
-      divChild.appendChild(checkIcon);
+      divInput.append(checkIcon);
 
       editIcon.id = "edit";
       editIcon.innerHTML = "Edit";
       editIcon.addEventListener("click", function() {
         paragraph.contentEditable = true;
-        paragraph.style.backgroundColor = "#fff";
       });
-      divChild.appendChild(editIcon);
+      divInput.append(editIcon);
 
       trashIcon.id = "delete";
       trashIcon.innerHTML = "Delete";
       trashIcon.addEventListener("click", function(){
-          divParent.remove();
+          divInput.remove();
       })       
-      input.appendChild(trashIcon);
-
-      toDoItems.appendChild(divChild);
+      divInput.append(trashIcon);
+      toDoItems.appendChild(divInput);
   
  }
  })
